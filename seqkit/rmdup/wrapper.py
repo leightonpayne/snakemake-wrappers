7efa95bd56763b6ffa106d8763fc462d5db774b1
leightonpayne <leightonpayne@gmail.com>
2023-03-03 15:42:28 +1300

    Update seqkit/rmdup wrapper

--- seqkit/rmdup/wrapper.py
+++ seqkit/rmdup/wrapper.py
@@ -1,8 +1,8 @@
 """Snakemake wrapper for removing duplicated sequences by ID/name/sequence using seqkit"""
 
 __author__ = "Leighton Payne"
-__copyright__ = "Copyright 2022, Leighton Payne"
-__email__ = "leightonpayne98@gmail.com"
+__copyright__ = "Copyright 2023, Leighton Payne"
+__email__ = "leighton@duck.com"
 __license__ = "MIT"
 
 from snakemake.shell import shell
@@ -11,10 +11,4 @@ import argparse
 extra = snakemake.params.get("extra", "")
 log = snakemake.log_fmt_shell(stdout=False, stderr=True)
 
-by = snakemake.params.get("by", "")
-if by == "sequence":
-    flag = "--by-seq"
-elif by == "name":
-    flag = "--by-name"
-
-shell("(seqkit rmdup {flag} {extra} {snakemake.input} > {snakemake.output}) {log}")
+shell("(seqkit rmdup {extra} {snakemake.input} > {snakemake.output}) {log}")
